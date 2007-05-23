1d886ae670820268587a0afc8d1239ac0f759d12
glasser <glasser@mit.edu>
2007-05-23 18:42:51 +0000

    Allow clients of NamedWindows to get at the name.

--- NamedWindows.hs
+++ NamedWindows.hs
@@ -1,4 +1,4 @@
-module XMonadContrib.NamedWindows ( NamedWindow, getName, withNamedWindow, unName ) where
+module XMonadContrib.NamedWindows ( NamedWindow, getName, withNamedWindow, unName, name ) where
 
 -- This module allows you to associate the X titles of windows with
 -- them.  See XMonadContrib.Mosaic for an example of its use.
@@ -25,6 +25,9 @@ getName w = asks display >>= \d -> do n <- maybe "" id `fmap` io (fetchName d w)
 
 unName :: NamedWindow -> Window
 unName (NW _ w) = w
+
+name :: NamedWindow -> String
+name (NW n _) = n
 
 withNamedWindow :: (NamedWindow -> X ()) -> X ()
 withNamedWindow f = do ws <- gets windowset
