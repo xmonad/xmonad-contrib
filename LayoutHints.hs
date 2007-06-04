8b95e7e2d37cc393ea168ec25b0c0d50e2553687
David Roundy <droundy@darcs.net>
2007-06-04 21:37:16 +0000

    add new LayoutHints module that makes layouts respect size hints.

---
+++ LayoutHints.hs
@@ -0,0 +1,21 @@
+module XMonadContrib.LayoutHints ( layoutHints ) where
+
+-- to use:
+-- defaultLayouts = [ layoutHints tiled, layoutHints $ mirror tiled , full ]
+
+import Operations ( applySizeHints )
+import Graphics.X11.Xlib
+import Graphics.X11.Xlib.Extras ( getWMNormalHints )
+import XMonad hiding ( trace )
+
+layoutHints :: Layout -> Layout
+layoutHints l = Layout { doLayout = \r x -> doLayout l r x >>= applyHints
+                       , modifyLayout = \x -> layoutHints `fmap` modifyLayout l x }
+
+applyHints :: [(Window, Rectangle)] -> X [(Window, Rectangle)]
+applyHints xs = mapM applyHint xs
+    where applyHint (w,Rectangle a b c d) =
+              withDisplay $ \disp ->
+                  do sh <- io $ getWMNormalHints disp w
+                     let (c',d') = applySizeHints sh (c,d)
+                     return (w, Rectangle a b c' d')
