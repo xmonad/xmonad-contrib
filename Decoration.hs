4f985379ec25f5210eae1ef7936b1ff3069a8c8e
David Roundy <droundy@darcs.net>
2007-06-10 16:36:51 +0000

    make Decoration draw the window initially.
    We still don't respond to expose events.  :(

--- Decoration.hs
+++ Decoration.hs
@@ -1,19 +1,17 @@
 module XMonadContrib.Decoration ( newDecoration ) where
 
 import qualified Data.Map as M
-
 import Control.Monad.Reader ( asks )
 import Control.Monad.State ( modify, gets )
-import Graphics.X11.Xlib ( Window, Rectangle(Rectangle), Pixel
-                         , createSimpleWindow, mapWindow, destroyWindow
-                         , buttonPress )
+import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras ( Event(AnyEvent,ButtonEvent), ev_subwindow, ev_event_type, ev_window )
 
 import XMonad
 import Operations ( ModifyWindows(ModifyWindows) )
 import qualified StackSet as W
 
-newDecoration :: Rectangle -> Int -> Pixel -> Pixel -> X () -> X () -> X Window
+newDecoration :: Rectangle -> Int -> Pixel -> Pixel
+              -> (Display -> Window -> GC -> X ()) -> X () -> X Window
 newDecoration (Rectangle x y w h) th fg bg draw click =
     do d <- asks display
        rt <- asks theRoot
@@ -21,7 +19,8 @@ newDecoration (Rectangle x y w h) th fg bg draw click =
        Just (l,ls) <- M.lookup n `fmap` gets layouts
        win <- io $ createSimpleWindow d rt x y w h (fromIntegral th) fg bg
        io $ mapWindow d win
-       let modml :: (SomeMessage -> X (Maybe Layout)) -> SomeMessage -> X (Maybe Layout)
+       let draw' = withGC win draw
+           modml :: (SomeMessage -> X (Maybe Layout)) -> SomeMessage -> X (Maybe Layout)
            modml oldml m | Just ModifyWindows == fromMessage m = io (destroyWindow d win) >> oldml m
                          | Just e <- fromMessage m = handle_event e >> oldml m
                          | otherwise = fmap modl `fmap` oldml m
@@ -31,7 +30,14 @@ newDecoration (Rectangle x y w h) th fg bg draw click =
                | t == buttonPress && thisw == win = click
            handle_event (ButtonEvent {ev_window = thisw,ev_event_type = t})
                | t == buttonPress && thisw == win = click
-           handle_event (AnyEvent {ev_window = thisw}) | thisw == win = draw
+           handle_event (AnyEvent {ev_window = thisw}) | thisw == win = draw'
            handle_event _ = return ()
+       draw'
        modify $ \s -> s { layouts = M.insert n (modl l,ls) (layouts s) }
        return win
+
+-- FIXME: withGC should use bracket (but can't, unless draw is an IO thing)
+withGC :: Drawable -> (Display -> Drawable -> GC -> X ()) -> X ()
+withGC w f = withDisplay $ \d -> do gc <- io $ createGC d w
+                                    f d w gc
+                                    io $ freeGC d gc
