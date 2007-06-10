fb8cddf4b44f6c0859468034136e0b198b34f4c8
dave <dave@nullcube.com>
2007-06-10 09:10:56 +0000

    greedydoc
    Add documentation on how to use GreedyView as your default workspace switcher.

--- GreedyView.hs
+++ GreedyView.hs
@@ -1,6 +1,27 @@
 -- greedyView is an alternative to standard workspace switching.  When a
 -- workspace is already visible on another screen, greedyView swaps the
 -- contents of that other screen with the current screen.
+--
+-- To use GreedyView as your default workspace switcher,
+--
+-- Add this import :
+--
+--      import XMonadContrib.GreedyView
+--
+-- And replace the function call used to switch workspaces,
+--
+-- this :
+--
+--  [((m .|. modMask, k), f i)
+--      | (i, k) <- zip [0 .. fromIntegral workspaces - 1] [xK_1 ..]
+--      , (f, m) <- [(view, 0), (shift, shiftMask)]]
+--
+-- becomes this :
+--
+--  [((m .|. modMask, k), f i)
+--      | (i, k) <- zip [0 .. fromIntegral workspaces - 1] [xK_1 ..]
+--      , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
+--
 
 module XMonadContrib.GreedyView (greedyView) where
 
