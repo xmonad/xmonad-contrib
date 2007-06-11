1a90e87933d7d842ef28bd971a4bf99c60b955c9
David Roundy <droundy@darcs.net>
2007-06-11 15:40:41 +0000

    add WorkspaceDir, which sets the current directory in a workspace.
    Actually, it sets the current directory in a layout, since there's no way I
    know of to attach a behavior to a workspace.  This means that any terminals
    (or other programs) pulled up in that workspace (with that layout) will
    execute in that working directory.  Sort of handy, I think.

---
+++ WorkspaceDir.hs
@@ -0,0 +1,35 @@
+module XMonadContrib.WorkspaceDir ( workspaceDir, changeDir ) where
+
+-- to use:
+
+-- import XMonadContrib.WorkspaceDir
+
+-- defaultLayouts = map (workspaceDir "~") [ tiled, ... ]
+
+-- In keybindings:
+--  , ((modMask .|. shiftMask, xK_x     ), changeDir ["~","/tmp"])
+
+import System.Directory ( setCurrentDirectory, getCurrentDirectory )
+import Data.List ( nub )
+
+import XMonad
+import Operations ( sendMessage )
+import XMonadContrib.Dmenu ( dmenu, runProcessWithInput )
+
+data Chdir = Chdir String deriving ( Typeable )
+instance Message Chdir
+
+workspaceDir :: String -> Layout -> Layout
+workspaceDir wd l = l { doLayout = \r x -> scd wd >> doLayout l r x
+                      , modifyLayout = ml }
+    where ml m | Just (Chdir wd') <- fromMessage m = return $ Just (workspaceDir wd' l)
+               | otherwise = fmap (workspaceDir wd) `fmap` modifyLayout l m
+
+scd :: String -> X ()
+scd x = do x' <- io (runProcessWithInput "bash" [] ("echo -n " ++ x) `catch` \_ -> return x)
+           safeIO $ setCurrentDirectory x'
+
+changeDir :: [String] -> X ()
+changeDir dirs = do thisd <- io getCurrentDirectory
+                    dir <- dmenu (nub (thisd:dirs))
+                    sendMessage (Chdir dir)
