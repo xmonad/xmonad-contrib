421a88c691ab81019ed5abb122113118b6fd33c3
Stefan O'Rear <stefanor@cox.net>
2007-06-11 00:54:07 +0000

    Fix LayoutHints in the presence of nonzero border widths

--- LayoutHints.hs
+++ LayoutHints.hs
@@ -3,10 +3,16 @@ module XMonadContrib.LayoutHints ( layoutHints ) where
 -- to use:
 -- defaultLayouts = [ layoutHints tiled, layoutHints $ mirror tiled , full ]
 
-import Operations ( applySizeHints )
+import Operations ( applySizeHints, D )
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras ( getWMNormalHints )
+import {-#SOURCE#-} Config (borderWidth)
 import XMonad hiding ( trace )
+
+-- | Expand a size by the given multiple of the border width.  The
+-- multiple is most commonly 1 or -1.
+adjBorders             :: Dimension -> D -> D
+adjBorders mult (w,h)  = (w+2*mult*borderWidth, h+2*mult*borderWidth)
 
 layoutHints :: Layout -> Layout
 layoutHints l = l { doLayout = \r x -> doLayout l r x >>= applyHints
@@ -17,5 +23,5 @@ applyHints xs = mapM applyHint xs
     where applyHint (w,Rectangle a b c d) =
               withDisplay $ \disp ->
                   do sh <- io $ getWMNormalHints disp w
-                     let (c',d') = applySizeHints sh (c,d)
+                     let (c',d') = adjBorders 1 . applySizeHints sh . adjBorders (-1) $ (c,d)
                      return (w, Rectangle a b c' d')
