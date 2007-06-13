0a0609bd155e8285b402c139678ff7cbd806ccda
Spencer Janssen <sjanssen@cse.unl.edu>
2007-06-13 03:53:23 +0000

    Update MagicFocus to the new StackSet

--- MagicFocus.hs
+++ MagicFocus.hs
@@ -7,5 +7,4 @@ magicFocus l = l { doLayout = \s -> (doLayout l) s . swap
                  , modifyLayout = \x -> fmap magicFocus `fmap` modifyLayout l x }
 
 swap :: Stack a -> Stack a
-swap Empty = Empty
-swap (Node f u d) = Node f [] (reverse u ++ d)
+swap (Stack f u d) = Stack f [] (reverse u ++ d)
