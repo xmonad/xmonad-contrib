72450b251234b4a3b32d6b2d0fa43c08cb21417c
Andrea Rossato <andrea.rossato@unibz.it>
2007-06-14 14:37:53 +0000

    Dmenu.hs: info and documentation

--- Dmenu.hs
+++ Dmenu.hs
@@ -1,10 +1,34 @@
-module XMonadContrib.Dmenu (dmenu, dmenuXinerama, runProcessWithInput) where
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  XMonadContrib.Dmenu
+-- Copyright   :  (c) Spencer Janssen <sjanssen@cse.unl.edu>
+-- License     :  BSD-style (see LICENSE)
+-- 
+-- Maintainer  :  Spencer Janssen <sjanssen@cse.unl.edu>
+-- Stability   :  unstable
+-- Portability :  unportable
+--
+-- A convenient binding to dmenu.
+--
+-----------------------------------------------------------------------------
+
+module XMonadContrib.Dmenu (
+                            -- * Usage
+                            -- $usage 
+                            dmenu, dmenuXinerama, 
+                            runProcessWithInput
+                           ) where
 
 import XMonad
 import qualified StackSet as W
 import System.Process
 import System.IO
 import Control.Monad.State
+
+-- $usage
+-- You can use this module with the following in your Config.hs file:
+--
+-- > import XMonadContrib.Dmenu
 
 runProcessWithInput :: FilePath -> [String] -> String -> IO String
 runProcessWithInput cmd args input = do
@@ -18,8 +42,8 @@ runProcessWithInput cmd args input = do
     waitForProcess ph
     return output
     
--- Starts dmenu on the current screen. Requires this patch to dmenu:
--- http://www.jcreigh.com/dmenu/dmenu-2.8-xinerama.patch
+-- | Starts dmenu on the current screen. Requires this patch to dmenu:
+-- http:\/\/www.jcreigh.com\/dmenu\/dmenu-2.8-xinerama.patch
 dmenuXinerama :: [String] -> X String
 dmenuXinerama opts = do
     curscreen <- (fromIntegral . W.screen . W.current) `liftM` gets windowset :: X Int
