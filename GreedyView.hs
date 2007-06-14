42865b0034ae36a073d101d0176f46d028266068
Andrea Rossato <andrea.rossato@unibz.it>
2007-06-14 14:39:53 +0000

    GreedyView.hs: info and documentation

--- GreedyView.hs
+++ GreedyView.hs
@@ -1,37 +1,52 @@
 {-# OPTIONS -fglasgow-exts #-}
--- Maintainer: Spencer Janssen <sjanssen@cse.unl.edu>
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  XMonadContrib.FindEmptyWorkspace
+-- Copyright   :  (c) Spencer Janssen <sjanssen@cse.unl.edu>
+-- License     :  BSD3-style (see LICENSE)
 --
--- greedyView is an alternative to standard workspace switching.  When a
--- workspace is already visible on another screen, greedyView swaps the
+-- Maintainer  :  Spencer Janssen <sjanssen@cse.unl.edu>
+-- Stability   :  unstable
+-- Portability :  unportable
+--
+-- GreedyView is an alternative to standard workspace switching.  When a
+-- workspace is already visible on another screen, GreedyView swaps the
 -- contents of that other screen with the current screen.
 --
--- To use GreedyView as your default workspace switcher,
---
--- Add this import :
---
---      import XMonadContrib.GreedyView
---
--- And replace the function call used to switch workspaces,
---
--- this :
---
---  [((m .|. modMask, k), f i)
---      | (i, k) <- zip [0 .. fromIntegral workspaces - 1] [xK_1 ..]
---      , (f, m) <- [(view, 0), (shift, shiftMask)]]
---
--- becomes this :
---
---  [((m .|. modMask, k), f i)
---      | (i, k) <- zip [0 .. fromIntegral workspaces - 1] [xK_1 ..]
---      , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
---
+-----------------------------------------------------------------------------
 
-module XMonadContrib.GreedyView (greedyView) where
+module XMonadContrib.GreedyView (
+                                 -- * Usage
+                                 -- $usage
+                                 greedyView
+                                ) where
 
 import StackSet as W hiding (filter)
 import XMonad
 import Operations
 import Data.List (find)
+
+-- $usage
+-- To use GreedyView as your default workspace switcher
+--
+-- Add this import:
+--
+-- >     import XMonadContrib.GreedyView
+--
+-- And replace the function call used to switch workspaces,
+--
+-- this:
+--
+-- > [((m .|. modMask, k), f i)
+-- >     | (i, k) <- zip [0 .. fromIntegral workspaces - 1] [xK_1 ..]
+-- >     , (f, m) <- [(view, 0), (shift, shiftMask)]]
+--
+-- becomes this :
+--
+-- > [((m .|. modMask, k), f i)
+-- >     | (i, k) <- zip [0 .. fromIntegral workspaces - 1] [xK_1 ..]
+-- >     , (f, m) <- [(greedyView, 0), (shift, shiftMask)]]
+--
 
 greedyView :: WorkspaceId -> X ()
 greedyView = windows . greedyView'
