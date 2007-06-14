3d8e47448f06632bf3000971cc96bc6885da19cc
Andrea Rossato <andrea.rossato@unibz.it>
2007-06-14 14:44:43 +0000

    WorkspaceDir.hs: info and documentation

--- WorkspaceDir.hs
+++ WorkspaceDir.hs
@@ -1,14 +1,29 @@
 {-# OPTIONS -fglasgow-exts #-}
-module XMonadContrib.WorkspaceDir ( workspaceDir, changeDir ) where
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  XMonadContrib.WorkspaceDir
+-- Copyright   :  (c) 2007  David Roundy <droundy@darcs.net>
+-- License     :  BSD3-style (see LICENSE)
+-- 
+-- Maintainer  :  David Roundy <droundy@darcs.net>
+-- Stability   :  unstable
+-- Portability :  unportable
+--
+-- WorkspaceDir is an exstension to set the current directory in a workspace.
+--
+-- Actually, it sets the current directory in a layout, since there's no way I
+-- know of to attach a behavior to a workspace.  This means that any terminals
+-- (or other programs) pulled up in that workspace (with that layout) will
+-- execute in that working directory.  Sort of handy, I think.
+--
+-----------------------------------------------------------------------------
 
--- to use:
-
--- import XMonadContrib.WorkspaceDir
-
--- defaultLayouts = map (workspaceDir "~") [ tiled, ... ]
-
--- In keybindings:
---  , ((modMask .|. shiftMask, xK_x     ), changeDir ["~","/tmp"])
+module XMonadContrib.WorkspaceDir ( 
+                                   -- * Usage
+                                   -- $usage
+                                   workspaceDir, 
+                                   changeDir
+                                  ) where
 
 import System.Directory ( setCurrentDirectory, getCurrentDirectory )
 import Data.List ( nub )
@@ -16,6 +31,18 @@ import Data.List ( nub )
 import XMonad
 import Operations ( sendMessage )
 import XMonadContrib.Dmenu ( dmenu, runProcessWithInput )
+
+-- $usage
+-- You can use this module with the following in your Config.hs file:
+--
+-- > import XMonadContrib.WorkspaceDir
+-- >
+-- > defaultLayouts = map (workspaceDir "~") [ tiled, ... ]
+--
+-- In keybindings:
+--
+-- >  , ((modMask .|. shiftMask, xK_x     ), changeDir ["~","/tmp"])
+
 
 data Chdir = Chdir String deriving ( Typeable )
 instance Message Chdir
