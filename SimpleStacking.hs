ede271f28b92700583a3ac60bcb367781dba79a3
Alec Berryman <alec@thened.net>
2007-07-04 20:06:26 +0000

    SimpleStacking: remove note about Xinerama incompatability, works fine now

--- SimpleStacking.hs
+++ SimpleStacking.hs
@@ -11,8 +11,6 @@
 --
 -- A module to be used to obtain a simple "memory" of stacking order.
 --
--- WARNING: This module is incompatible with Xinerama!
---
 -----------------------------------------------------------------------------
 
 module XMonadContrib.SimpleStacking (
