56a40fc39fa43799116e4939f6f3e0cb877b8450
Andrea Rossato <andrea.rossato@unibz.it>
2007-08-02 16:53:36 +0000

    XMonadPrompt: a graphical prompt for running XMonad internal commands

---
+++ XMonadPrompt.hs
@@ -0,0 +1,41 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  XMonadContrib.XMonadPrompt
+-- Copyright   :  (C) 2007 Andrea Rossato
+-- License     :  BSD3
+-- 
+-- Maintainer  :  andrea.rossato@unibz.it
+-- Stability   :  unstable
+-- Portability :  unportable
+--
+-- A prompt for running XMonad commands
+--
+-----------------------------------------------------------------------------
+
+module XMonadContrib.XMonadPrompt (
+                             -- * Usage
+                             -- $usage
+                             xmonadPrompt
+                              ) where
+{-
+usage:
+in Config.hs add:
+> import XMonadContrib.XMonadPrompt
+
+in you keybindings add:
+
+>   , ((modMask .|. controlMask, xK_x), xmonadPrompt defaultPromptConfig)
+
+-}
+
+import XMonad
+import XMonadContrib.XPrompt
+import XMonadContrib.Commands
+
+data XMonad = XMonad
+
+instance XPrompt XMonad where
+    showXPrompt XMonad = "XMonad:   "
+
+xmonadPrompt :: XPConfig -> X ()
+xmonadPrompt c = mkXPrompt XMonad c (mkComplFunFromList (map fst defaultCommands)) runCommand'
