e1a10b926e41b53e7ebc54e184d68bb3ef1ba929
nornagon <nornagon@gmail.com>
2007-10-06 22:41:56 +0000

    Replace -fglasgow-exts with LANGUAGE pragma in WindowNavigation.hs

--- WindowNavigation.hs
+++ WindowNavigation.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS -fglasgow-exts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  XMonadContrib.WorkspaceDir
