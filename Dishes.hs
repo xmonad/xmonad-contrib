f06195dd47f131d2e6434d2ffdb9b8ff70baadd1
nornagon <nornagon@gmail.com>
2007-10-06 22:21:55 +0000

    Replace -fglasgow-exts with LANGUAGE pragma in Dishes.hs

--- Dishes.hs
+++ Dishes.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -fglasgow-exts #-}
+{-# LANGUAGE FlexibleInstances #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  XMonadContrib.Dishes
