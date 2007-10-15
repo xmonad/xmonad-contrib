a1a2f6b6c385282235aac8d2af867154acd18274
David Roundy <droundy@darcs.net>
2007-10-15 16:15:29 +0000

    fix float bug in CopyWindow.

--- CopyWindow.hs
+++ CopyWindow.hs
@@ -76,4 +76,4 @@ kill1 = do ss <- gets windowset
            whenJust (peek ss) $ \w -> if member w $ delete'' w ss
                                       then windows $ delete'' w
                                       else kill
-    where delete'' w = sink w . modify Nothing (filter (/= w))
+    where delete'' w = modify Nothing (filter (/= w))
