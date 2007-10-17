c437e3e384cde1c2f7d25b979a2a91b7d5fa6408
Shachaf Ben-Kiki <shachaf@gmail.com>
2007-10-17 19:46:22 +0000

    Fix LANGUAGE pragmas

--- Roledex.hs
+++ Roledex.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TypeSynonymInstances #-}
 
 -----------------------------------------------------------------------------
 -- |
--- TilePrime.hs
+++ TilePrime.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances #-}
 -- --------------------------------------------------------------------------
 -- -- |
 -- -- Module      :  TilePrime.hs
