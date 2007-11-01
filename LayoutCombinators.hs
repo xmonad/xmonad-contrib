0a242c6a6eabbd689514e23d6bffbd32ad322e3a
David Roundy <droundy@darcs.net>
2007-11-01 18:11:28 +0000

    reenable JumpToLayout in NewSelect.

--- LayoutCombinators.hs
+++ LayoutCombinators.hs
@@ -17,14 +17,13 @@
 module XMonadContrib.LayoutCombinators (
     -- * Usage
     -- $usage
-    (<|>), (</>), (<||>), (<//>), (|||)
+    (<|>), (</>), (<||>), (<//>), (|||), JumpToLayout(JumpToLayout)
     ) where
 
 import Data.Maybe ( isJust )
 
 import XMonad
-import Operations ( Tall(..), Mirror(..),
-                    ChangeLayout(NextLayout,JumpToLayout), LayoutMessages(..) )
+import Layouts ( Tall(..), Mirror(..), ChangeLayout(NextLayout) )
 import XMonadContrib.Combo
 import XMonadContrib.DragPane
 
@@ -50,6 +49,9 @@ data NewSelect l1 l2 a = NewSelect Bool (l1 a) (l2 a) deriving ( Read, Show )
 
 data NoWrap = NextLayoutNoWrap | Wrap deriving ( Read, Show, Typeable )
 instance Message NoWrap
+
+data JumpToLayout = JumpToLayout String deriving ( Read, Show, Typeable )
+instance Message JumpToLayout
 
 instance (LayoutClass l1 a, LayoutClass l2 a) => LayoutClass (NewSelect l1 l2) a where
     doLayout (NewSelect True l1 l2) r s = do (wrs, ml1') <- doLayout l1 r s
@@ -81,6 +83,36 @@ instance (LayoutClass l1 a, LayoutClass l2 a) => LayoutClass (NewSelect l1 l2) a
                       Nothing -> do ml2' <- handleMessage l2 (SomeMessage Hide)
                                     ml1' <- handleMessage l1 (SomeMessage Wrap)
                                     return $ Just $ NewSelect True (maybe l1 id ml1') (maybe l2 id ml2')
+    handleMessage (NewSelect True l1 l2) m
+        | Just (JumpToLayout d) <- fromMessage m,
+          d == description l2 = do ml1' <- handleMessage l1 (SomeMessage Hide)
+                                   return $ Just $ NewSelect False (maybe l1 id ml1') l2
+    handleMessage (NewSelect True l1 l2) m
+        | Just (JumpToLayout d) <- fromMessage m
+        = do ml1' <- handleMessage l1 m
+             case ml1' of
+               Just l1' -> return $ Just $ NewSelect True l1' l2
+               Nothing ->
+                   do ml2' <- handleMessage l2 m
+                      case ml2' of
+                        Nothing -> return Nothing
+                        Just l2' -> do ml1'' <- handleMessage l1 (SomeMessage Hide)
+                                       return $ Just $ NewSelect False (maybe l1 id ml1'') l2'
+    handleMessage (NewSelect False l1 l2) m
+        | Just (JumpToLayout d) <- fromMessage m,
+          d == description l1 = do ml2' <- handleMessage l2 (SomeMessage Hide)
+                                   return $ Just $ NewSelect True l1 (maybe l2 id ml2')
+    handleMessage (NewSelect False l1 l2) m
+        | Just (JumpToLayout d) <- fromMessage m
+        = do ml2' <- handleMessage l2 m
+             case ml2' of
+               Just l2' -> return $ Just $ NewSelect False l1 l2'
+               Nothing ->
+                   do ml1' <- handleMessage l1 m
+                      case ml1' of
+                        Nothing -> return Nothing
+                        Just l1' -> do ml2'' <- handleMessage l2 (SomeMessage Hide)
+                                       return $ Just $ NewSelect True l1' (maybe l2 id ml2'')
     handleMessage (NewSelect b l1 l2) m
         | Just ReleaseResources  <- fromMessage m =
         do ml1' <- handleMessage l1 m
