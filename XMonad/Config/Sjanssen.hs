9cd33f809ea8259c53fae6ab0124b7c38b2f615c
Spencer Janssen <sjanssen@cse.unl.edu>
2007-11-05 05:50:22 +0000

    Minor updates to Sjanssen.hs

--- XMonad/Config/Sjanssen.hs
+++ XMonad/Config/Sjanssen.hs
@@ -16,6 +16,7 @@ import Data.Bits
 import qualified Data.Map as M
 import Graphics.X11
 
+sjanssenConfig :: XConfig
 sjanssenConfig = defaultConfig
         { defaultGaps = [(15,0,0,0)]
         , terminal = "urxvt"
@@ -33,16 +34,7 @@ sjanssenConfig = defaultConfig
     mykeys (XConfig {modMask = modMask}) = M.fromList $
         [((modMask,               xK_p     ), shellPrompt myPromptConfig)]
     -- default tiling algorithm partitions the screen into two panes
-    tiled   = Tall nmaster delta ratio
-
-    -- The default number of windows in the master pane
-    nmaster = 1
-
-    -- Default proportion of screen occupied by master pane
-    ratio   = 1%2
-
-    -- Percent of screen to increment by when resizing panes
-    delta   = 3%100
+    tiled   = Tall 1 0.5 0.03
 
 myPromptConfig = defaultXPConfig
                     { position = Top
