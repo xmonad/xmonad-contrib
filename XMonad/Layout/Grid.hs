ef6ef321cbbed66f5034a15fb0f67f3567f1d3d5
Devin Mullins <me@twifkak.com>
2007-11-17 20:11:40 +0000

    Grid: tabs -> spaces

--- XMonad/Layout/Grid.hs
+++ XMonad/Layout/Grid.hs
@@ -43,23 +43,23 @@ instance LayoutClass Grid a where
 
 arrange :: Rectangle -> [a] -> [(a, Rectangle)]
 arrange (Rectangle rx ry rw rh) st = zip st rectangles
-	where
-	nwins = length st
-	ncols = ceiling . (sqrt :: Double -> Double) . fromIntegral $ nwins
-	mincs = nwins `div` ncols
-	extrs = nwins - ncols * mincs
-	chop :: Int -> Dimension -> [(Position, Dimension)]
-	chop n m = ((0, m - k * fromIntegral (pred n)) :) . map (flip (,) k) . tail . reverse . take n . tail . iterate (subtract k') $ m'
-		where
-		k :: Dimension
-		k = m `div` fromIntegral n
-		m' = fromIntegral m
-		k' :: Position
-		k' = fromIntegral k
-	xcoords = chop ncols rw
-	ycoords = chop mincs rh
-	ycoords' = chop (succ mincs) rh
-	(xbase, xext) = splitAt (ncols - extrs) xcoords
-	rectangles = combine ycoords xbase ++ combine ycoords' xext
-		where
-		combine ys xs = [Rectangle (rx + x) (ry + y) w h | (x, w) <- xs, (y, h) <- ys]
+        where
+        nwins = length st
+        ncols = ceiling . (sqrt :: Double -> Double) . fromIntegral $ nwins
+        mincs = nwins `div` ncols
+        extrs = nwins - ncols * mincs
+        chop :: Int -> Dimension -> [(Position, Dimension)]
+        chop n m = ((0, m - k * fromIntegral (pred n)) :) . map (flip (,) k) . tail . reverse . take n . tail . iterate (subtract k') $ m'
+                where
+                k :: Dimension
+                k = m `div` fromIntegral n
+                m' = fromIntegral m
+                k' :: Position
+                k' = fromIntegral k
+        xcoords = chop ncols rw
+        ycoords = chop mincs rh
+        ycoords' = chop (succ mincs) rh
+        (xbase, xext) = splitAt (ncols - extrs) xcoords
+        rectangles = combine ycoords xbase ++ combine ycoords' xext
+                where
+                combine ys xs = [Rectangle (rx + x) (ry + y) w h | (x, w) <- xs, (y, h) <- ys]
