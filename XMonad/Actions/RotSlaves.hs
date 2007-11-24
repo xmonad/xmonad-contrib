93fd3f0f7d67dce16522167b19ad64ad92c9cfc8
Brent Yorgey <byorgey@gmail.com>
2007-11-24 17:45:18 +0000

    RotSlaves: haddock updates

--- XMonad/Actions/RotSlaves.hs
+++ XMonad/Actions/RotSlaves.hs
@@ -8,8 +8,8 @@
 -- Stability    : unstable
 -- Portability  : unportable
 --
--- Rotate all windows except the master window
--- and keep the focus in place.
+-- Rotate all windows except the master window and keep the focus in
+-- place.
 -----------------------------------------------------------------------------
 module XMonad.Actions.RotSlaves (
 	-- $usage
@@ -27,22 +27,24 @@ import XMonad
 --
 -- > import XMonad.Actions.RotSlaves
 --
--- and add a keybinding:
+-- and add whatever keybindings you would like, for example:
 --
--- > , ((modMask .|. shiftMask, xK_Tab   ), rotSlavesUp)
+-- > , ((modMask x .|. shiftMask, xK_Tab   ), rotSlavesUp)
 --
+-- This operation will rotate all windows except the master window,
+-- while the focus stays where it is. It is useful together with the
+-- TwoPane layout (see "XMonad.Layout.TwoPane").
 --
--- This operation will rotate all windows except the master window, while the focus
--- stays where it is. It is useful together with the TwoPane-Layout (see "XMonad.Layout.TwoPane").
+-- For detailed instructions on editing your key bindings, see
+-- "XMonad.Doc.Extending#Editing_key_bindings".
 
--- %import XMonad.Actions.RotSlaves
--- %keybind , ((modMask .|. shiftMask, xK_Tab   ), rotSlavesUp)
-
--- | Rotate the windows in the current stack excluding the first one
+-- | Rotate the windows in the current stack, excluding the first one
+--   (master).
 rotSlavesUp,rotSlavesDown :: X ()
 rotSlavesUp   = windows $ modify' (rotSlaves' (\l -> (tail l)++[head l]))
 rotSlavesDown = windows $ modify' (rotSlaves' (\l -> [last l]++(init l)))
 
+-- | The actual rotation, as a pure function on the window stack.
 rotSlaves' :: ([a] -> [a]) -> Stack a -> Stack a
 rotSlaves' _ s@(Stack _ [] []) = s
 rotSlaves' f   (Stack t [] rs) = Stack t [] (f rs)                -- Master has focus
@@ -50,11 +52,12 @@ rotSlaves' f s@(Stack _ ls _ ) = Stack t' (reverse revls') rs'    -- otherwise
     where  (master:ws)     = integrate s
            (revls',t':rs') = splitAt (length ls) (master:(f ws))
 
--- | Rotate the windows in the current stack
+-- | Rotate all the windows in the current stack.
 rotAllUp,rotAllDown :: X ()
 rotAllUp   = windows $ modify' (rotAll' (\l -> (tail l)++[head l]))
 rotAllDown = windows $ modify' (rotAll' (\l -> [last l]++(init l)))
 
+-- | The actual rotation, as a pure function on the window stack.
 rotAll' :: ([a] -> [a]) -> Stack a -> Stack a
 rotAll' f s = Stack r (reverse revls) rs
     where (revls,r:rs) = splitAt (length (up s)) (f (integrate s))
