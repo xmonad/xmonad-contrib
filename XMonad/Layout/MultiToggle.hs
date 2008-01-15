670d3160c456383c8113a41c8ad602bdda0f4158
Brent Yorgey <byorgey@gmail.com>
2008-01-15 19:33:11 +0000

    MultiToggle.hs: improve 'description' implementation in LayoutClass instance to display the current transformed layout rather than just 'MultiToggle'

--- XMonad/Layout/MultiToggle.hs
+++ XMonad/Layout/MultiToggle.hs
@@ -81,7 +81,7 @@ import Data.Maybe
 -- > data NOBORDERS = NOBORDERS deriving (Read, Show, Eq, Typeable)
 -- > instance Transformer NOBORDERS Window where
 -- >     transform _ x k = k (noBorders x)
--- > 
+-- >
 -- > data FULL = FULL deriving (Read, Show, Eq, Typeable)
 -- > instance Transformer FULL Window where
 -- >     transform _ x k = k Full
@@ -189,7 +189,7 @@ acceptChange :: (LayoutClass l' a) => MultiToggle ts l a -> ((l' a -> MultiToggl
 acceptChange mt f = fmap (f (\x -> mt{ currLayout = EL x }))
 
 instance (Typeable a, Show ts, HList ts a, LayoutClass l a) => LayoutClass (MultiToggle ts l) a where
-    description _ = "MultiToggle"
+    description (MultiToggle { currLayout = (EL l) }) = description l
 
     pureLayout mt r s = currLayout mt `unEL` \l -> pureLayout l r s
 
