31d303508e7e3658f810ef4040340da169deb58d
Spencer Janssen <sjanssen@cse.unl.edu>
2008-03-25 21:38:18 +0000

    I don't use DwmStyle

--- XMonad/Config/Sjanssen.hs
+++ XMonad/Config/Sjanssen.hs
@@ -13,7 +13,6 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Prompt
 import XMonad.Prompt.Shell
 import XMonad.Util.Run (spawnPipe)
-import XMonad.Layout.DwmStyle
 
 import qualified Data.Map as M
 import System.IO (hPutStrLn)
@@ -30,7 +29,7 @@ sjanssenConfig = do
                 , ((modm, button2), (\w -> focus w >> windows W.swapMaster))
                 , ((modm.|. shiftMask, button1), (\w -> focus w >> mouseResizeWindow w)) ]
         , keys = \c -> mykeys c `M.union` keys defaultConfig c
-        , layoutHook = dwmStyle shrinkText myTheme $ avoidStruts $ smartBorders (tiled Tall ||| tiled Wide ||| Full ||| tabbed shrinkText myTheme)
+        , layoutHook = avoidStruts $ smartBorders (tiled Tall ||| tiled Wide ||| Full ||| tabbed shrinkText myTheme)
         , manageHook = manageHook defaultConfig <+> manageDocks
         }
  where
