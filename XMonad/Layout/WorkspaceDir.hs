e3d455ded447892c25389c495cdaff026ab79fe4
David Roundy <droundy@darcs.net>
2008-03-26 15:27:08 +0000

    make workspaceDir work even in workspaces with no windows.
    This also fixes a (minor) bug when the focussed window is present on
    multiple visible workspaces.

--- XMonad/Layout/WorkspaceDir.hs
+++ XMonad/Layout/WorkspaceDir.hs
@@ -69,9 +69,9 @@ instance Message Chdir
 data WorkspaceDir a = WorkspaceDir String deriving ( Read, Show )
 
 instance LayoutModifier WorkspaceDir Window where
-    redoLayout (WorkspaceDir d) _ s wrs = do w <- gets windowset
-                                             when (Just (focus s) == peek w) $ scd d
-                                             return (wrs, Nothing)
+    modifyLayout (WorkspaceDir d) w r = do tc <- gets (tag . current . windowset)
+                                           when (tc == tag w) $ scd d
+                                           runLayout w r
     handleMess (WorkspaceDir _) m
         | Just (Chdir wd) <- fromMessage m = do wd' <- cleanDir wd
                                                 return $ Just $ WorkspaceDir wd'
