90d5b56d459ddabeae6ef786faaa43b25f495187
Spencer Janssen <sjanssen@cse.unl.edu>
2008-05-07 02:25:16 +0000

    Add ToggleStruts to the desktop config

--- XMonad/Config/Desktop.hs
+++ XMonad/Config/Desktop.hs
@@ -23,11 +23,17 @@ import XMonad.Config (defaultConfig)
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
 
+import qualified Data.Map as M
+
 desktopConfig = defaultConfig
                 { logHook    = ewmhDesktopsLogHook
                 , layoutHook = desktopLayoutModifiers $ layoutHook defaultConfig
                 , manageHook = manageHook defaultConfig <+> manageDocks
+                , keys       = \c -> desktopKeys c `M.union` keys defaultConfig c
                 }
+
+desktopKeys (XConfig {modMask = modm}) = M.fromList $
+    [ ((modm, xK_b), sendMessage ToggleStruts) ]
 
 desktopLayoutModifiers = avoidStruts . ewmhDesktopsLayout
 
