efc2f1160fcf3703f6eafa006b49295a4eaba657
Brent Yorgey <byorgey@cis.upenn.edu>
2009-02-21 23:08:50 +0000

    add some boilerplate comments at the top of IndependentScreens

--- XMonad/Layout/IndependentScreens.hs
+++ XMonad/Layout/IndependentScreens.hs
@@ -1,3 +1,18 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  XMonad.Layout.IndependentScreens
+-- Copyright   :  (c) 2009 Daniel Wagner
+-- License     :  BSD3
+--
+-- Maintainer  :  <daniel@wagner-home.com>
+-- Stability   :  unstable
+-- Portability :  unportable
+--
+-- Utility functions for simulating independent sets of workspaces on
+-- each screen (like dwm's workspace model), using internal tags to
+-- distinguish workspaces associated with each screen.
+-----------------------------------------------------------------------------
+
 module IndependentScreens where
 
 marshall (S sc) ws = show sc ++ '_':ws
