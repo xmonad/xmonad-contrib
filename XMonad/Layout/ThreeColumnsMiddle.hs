cc0e18af8a35453619873f9d9cd8bc0370870db6
Adam Vogt <vogt.adam@gmail.com>
2009-04-25 16:17:10 +0000

    Export ThreeColMid from L.ThreeColumnsMiddle
    The configs that import it should continue to work with this module, though the
    type of the ThreeColMid constructor is now ThreeCol (previously ThreeColMid).

---
+++ XMonad/Layout/ThreeColumnsMiddle.hs
@@ -0,0 +1,4 @@
+module XMonad.Layout.ThreeColumnsMiddle {-# DEPRECATED "Import XMonad.Layout.ThreeColumns instead" #-}
+    (module XMonad.Layout.ThreeColumns) where
+
+import XMonad.Layout.ThreeColumns (ThreeCol(ThreeColMid))
