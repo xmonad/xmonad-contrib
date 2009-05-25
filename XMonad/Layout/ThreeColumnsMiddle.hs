3f3aff573a209e3e953516b36c01cb0b787de58e
Adam Vogt <vogt.adam@gmail.com>
2009-05-25 00:32:45 +0000

    Remove L.ThreeColumnsMiddle compatiblity module
    Signed off here too:
    http://www.haskell.org/pipermail/xmonad/2009-May/007883.html

--- XMonad/Layout/ThreeColumnsMiddle.hs
+++
@@ -1,4 +0,0 @@
-module XMonad.Layout.ThreeColumnsMiddle {-# DEPRECATED "Import XMonad.Layout.ThreeColumns instead" #-}
-    (module XMonad.Layout.ThreeColumns) where
-
-import XMonad.Layout.ThreeColumns (ThreeCol(ThreeColMid))
