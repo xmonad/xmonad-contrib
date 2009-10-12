3e0be7dd1b455c57d9ef9814cd1e4f29857d26fc
Adam Vogt <vogt.adam@gmail.com>
2009-10-12 04:24:57 +0000

    Use LANGUAGE pragma instead of -fglasgow-exts in L.Minimize

--- XMonad/Layout/Minimize.hs
+++ XMonad/Layout/Minimize.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -fglasgow-exts #-} -- For deriving Data/Typeable
+{-# LANGUAGE MultiParamTypeClasses, DeriveDataTypeable, TypeSynonymInstances, FlexibleContexts #-}
 ----------------------------------------------------------------------------
 -- |
 -- Module      :  XMonad.Layout.Minimize
