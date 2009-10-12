bf36bb785aefd2a246dc493b67c7e733fab1d539
Adam Vogt <vogt.adam@gmail.com>
2009-10-12 04:31:33 +0000

    Correct erroneous haddock link in U.XSelection

--- XMonad/Util/XSelection.hs
+++ XMonad/Util/XSelection.hs
@@ -133,7 +133,7 @@ safePromptSelection app = join $ io $ liftM (safeSpawn app . return) getSelectio
 unsafePromptSelection app = join $ io $ liftM unsafeSpawn $ fmap (\x -> app ++ " " ++ x) getSelection
 
 {- | A wrapper around 'promptSelection' and its safe variant. They take two parameters, the first is a function that transforms strings, and the second is the application to run. The transformer essentially transforms the selection in X.
-One example is to wrap code, such as a command line action copied out of the browser to be run as '"sudo" ++ cmd' or '"su - -c \"" ++ cmd ++ "\"".
+One example is to wrap code, such as a command line action copied out of the browser to be run as @"sudo" ++ cmd@ or @"su - -c \""++ cmd ++"\""@.
 -}
 transformPromptSelection, transformSafePromptSelection :: (String -> String) -> String -> X ()
 transformPromptSelection f app = join $ io $ liftM (safeSpawn app . return) (fmap f getSelection)
