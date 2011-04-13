08c4c09fc59e5a382e712ada38262efeaa432e53
Brandon S Allbery KF8NH <allbery.b@gmail.com>
2011-04-13 05:30:02 +0000

    Typo in window-properties.sh
    Somewhere between my creating the original version of this script and
    adding it to the tree, a backslash got lost.  It appears to have been
    lost in the version I put on the wiki, so I suspect a copy-paste
    problem at that point.

--- scripts/window-properties.sh
+++ scripts/window-properties.sh
@@ -9,7 +9,7 @@
 
 exec xprop -notype \
   -f WM_NAME        8s ':\n  title =\? $0\n' \
-  -f WM_CLASS       8s ':\n  appName =\? $0\n  className =? $1\n' \
+  -f WM_CLASS       8s ':\n  appName =\? $0\n  className =\? $1\n' \
   -f WM_WINDOW_ROLE 8s ':\n  stringProperty "WM_WINDOW_ROLE" =\? $0\n' \
   WM_NAME WM_CLASS WM_WINDOW_ROLE \
   ${1+"$@"}
