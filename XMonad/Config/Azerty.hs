7629f774c602ca9005b4ff3bc871d68e1dfff5c9
Alexandre Px <al@px.io>
2016-09-07 21:54:09 +0200

    fix Couldn't match expected type `KeyMask'

--- XMonad/Config/Azerty.hs
+++ XMonad/Config/Azerty.hs
@@ -49,6 +49,6 @@ azertyKeys conf@(XConfig {modMask = modm}) = M.fromList $
     ++
     -- mod-{z,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{z,e,r} %! Move client to screen 1, 2, or 3
-    [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_z, xK_e, xK_r] [0..],
           (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
