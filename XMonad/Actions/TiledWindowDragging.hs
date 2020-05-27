b2446cd633df481f61f055fef52f0937080c05d9
Leon Kowarschick <lkowarschick@gmail.com>
2020-05-27 18:01:01 +0200

    Adjust code style to match guidelines

--- XMonad/Actions/TiledWindowDragging.hs
+++ XMonad/Actions/TiledWindowDragging.hs
@@ -48,50 +48,49 @@ import           Control.Monad
 -- You need "XMonad.Layout.DraggingVisualizer" for this to look good.
 dragWindow :: Window -> X ()
 dragWindow window = whenX (isClient window) $ do
-  focus window
-  (offsetX, offsetY)                <- getPointerOffset window
-  (winX, winY, winWidth, winHeight) <- getWindowPlacement window
+    focus window
+    (offsetX, offsetY)                <- getPointerOffset window
+    (winX, winY, winWidth, winHeight) <- getWindowPlacement window
 
-  mouseDrag
-    (\posX posY ->
-      let rect = Rectangle (fInt (fInt winX + (posX - fInt offsetX)))
-                           (fInt (fInt winY + (posY - fInt offsetY)))
-                           (fInt winWidth)
-                           (fInt winHeight)
-      in  sendMessage $ DraggingWindow window rect
-    )
-    (sendMessage DraggingStopped >> performWindowSwitching window)
+    mouseDrag
+        (\posX posY ->
+          let rect = Rectangle (fInt (fInt winX + (posX - fInt offsetX)))
+                               (fInt (fInt winY + (posY - fInt offsetY)))
+                               (fInt winWidth)
+                               (fInt winHeight)
+          in  sendMessage $ DraggingWindow window rect
+        )
+        (sendMessage DraggingStopped >> performWindowSwitching window)
 
 
 -- | get the pointer offset relative to the given windows root coordinates
 getPointerOffset :: Window -> X (Int, Int)
 getPointerOffset win = do
-  (_, _, _, oX, oY, _, _, _) <- withDisplay (\d -> io $ queryPointer d win)
-  return (fInt oX, fInt oY)
+    (_, _, _, oX, oY, _, _, _) <- withDisplay (\d -> io $ queryPointer d win)
+    return (fInt oX, fInt oY)
 
 -- | return a tuple of windowX, windowY, windowWidth, windowHeight
 getWindowPlacement :: Window -> X (Int, Int, Int, Int)
 getWindowPlacement window = do
-  wa <- withDisplay (\d -> io $ getWindowAttributes d window)
-  return
-    (fInt $ wa_x wa, fInt $ wa_y wa, fInt $ wa_width wa, fInt $ wa_height wa)
+    wa <- withDisplay (\d -> io $ getWindowAttributes d window)
+    return (fInt $ wa_x wa, fInt $ wa_y wa, fInt $ wa_width wa, fInt $ wa_height wa)
 
 
 performWindowSwitching :: Window -> X ()
 performWindowSwitching win = do
-  root                          <- asks theRoot
-  (_, _, selWin, _, _, _, _, _) <- withDisplay (\d -> io $ queryPointer d root)
-  ws                            <- gets windowset
-  let allWindows = W.index ws
-  when ((win `elem` allWindows) && (selWin `elem` allWindows)) $ do
-    let allWindowsSwitched = map (switchEntries win selWin) allWindows
-    let (ls, t : rs)       = break (== win) allWindowsSwitched
-    let newStack           = W.Stack t (reverse ls) rs
-    windows $ W.modify' $ const newStack
- where
-  switchEntries a b x | x == a    = b
-                      | x == b    = a
-                      | otherwise = x
+    root                          <- asks theRoot
+    (_, _, selWin, _, _, _, _, _) <- withDisplay (\d -> io $ queryPointer d root)
+    ws                            <- gets windowset
+    let allWindows = W.index ws
+    when ((win `elem` allWindows) && (selWin `elem` allWindows)) $ do
+        let allWindowsSwitched = map (switchEntries win selWin) allWindows
+        let (ls, t : rs)       = break (== win) allWindowsSwitched
+        let newStack           = W.Stack t (reverse ls) rs
+        windows $ W.modify' $ const newStack
+   where
+    switchEntries a b x | x == a    = b
+                        | x == b    = a
+                        | otherwise = x
 
 
 
