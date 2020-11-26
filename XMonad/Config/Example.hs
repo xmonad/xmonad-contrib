d18bcdc1658b212aa1fb15a1d5e2e46cd774aa79
PRESFIL <PRESFIL@users.noreply.github.com>
2020-11-26 13:23:33 +0000

    Replace composeOne with composeAll

--- XMonad/Config/Example.hs
+++ XMonad/Config/Example.hs
@@ -68,10 +68,10 @@ myXPConfig = def
 --
 -- Use the `xprop' tool to get the info you need for these matches.
 -- For className, use the second value that xprop gives you.
-myManageHook = composeOne
-  [ className =? "Pidgin" -?> doFloat
-  , className =? "XCalc"  -?> doFloat
-  , className =? "mpv"    -?> doFloat
+myManageHook = composeAll
+  [ className =? "Pidgin" --> doFloat
+  , className =? "XCalc"  --> doFloat
+  , className =? "mpv"    --> doFloat
   ] <+> composeOne
   -- Handle floating windows:
   [ transience            -- move transient windows to their parent
