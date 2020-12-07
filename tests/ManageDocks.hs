7ef0faa986a7e1fadda6d508ea33f06703ef4842
Yecine Megdiche <yecine.megdiche@gmail.com>
2020-12-07 20:11:24 +0100

    added new test-suite

--- tests/ManageDocks.hs
+++ tests/ManageDocks.hs
@@ -1,18 +1,6 @@
 module ManageDocks where
-import XMonad
-import XMonad.Hooks.ManageDocks
-import Test.QuickCheck
-import Foreign.C.Types
-import Properties
-
-instance Arbitrary CLong where
-    arbitrary = fromIntegral <$> (arbitrary :: Gen Int)
-instance Arbitrary RectC where
-    arbitrary = do
-        (x,y) <- arbitrary
-        NonNegative w <- arbitrary
-        NonNegative h <- arbitrary
-        return $ RectC (x,y,x+w,y+h)
+import           XMonad                         ( Rectangle )
+import           XMonad.Hooks.ManageDocks
 
 prop_r2c_c2r :: RectC -> Bool
 prop_r2c_c2r r = r2c (c2r r) == r
