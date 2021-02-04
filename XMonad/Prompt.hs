xmonad/xmonad-contrib

commit 526336ecec98aa2fe1cc98521473372b0664e19d

Only modify the last word during tab completion
Fixes #164.

It's not clear to me why these functions take in XPState as a param only
to then produce some XP () values. It seems like they could just as well
call `get`.

XMonad/Prompt.hs
@@ -698,9 +698,8 @@ handleCompletion cs = do
 
     let updateWins  l = redrawWindows l
         updateState l = case alwaysHlight of
-            False                                           -> simpleComplete l st
-            True | Just (command st) /= highlightedCompl st -> alwaysHighlightCurrent st
-                 | otherwise                                -> alwaysHighlightNext l st
+            False -> simpleComplete l st
+            True  -> hlComplete l st
 
     case cs of
       []  -> updateWindows
@@ -720,33 +719,31 @@ handleCompletion cs = do
                            , highlightedCompl = Just newCommand
                            }
 
-        -- If alwaysHighlight is on, and this is the first use of the
-        -- completion key, update the buffer so that it contains the
-        -- current completion item.
-        alwaysHighlightCurrent :: XPState -> XP ()
-        alwaysHighlightCurrent st = do
-          let newCommand = fromMaybe (command st) $ highlightedItem st cs
-          modify $ \s -> setCommand newCommand $
-                         setHighlightedCompl (Just newCommand) $
-                         s { offset = length newCommand
-                           }
-
         -- If alwaysHighlight is on, and the user wants the next
         -- completion, move to the next completion item and update the
         -- buffer to reflect that.
         --
         --TODO: Scroll or paginate results
-        alwaysHighlightNext :: [String] -> XPState -> XP ()
-        alwaysHighlightNext l st = do
-          let complIndex' = nextComplIndex st (length l)
-              highlightedCompl' = highlightedItem st { complIndex = complIndex'} cs
-              newCommand = fromMaybe (command st) $ highlightedCompl'
-          modify $ \s -> setHighlightedCompl highlightedCompl' $
-                         setCommand newCommand $
-                         s { complIndex = complIndex'
-                           , offset = length newCommand
-                           }
-
+        hlComplete :: [String] -> XPState -> XP ()
+        hlComplete l st =
+          let newCommand = fromMaybe (command st) $ highlightedItem st l in
+          if newCommand == (getLastWord . command $ st)
+            then do
+              -- The word currently under the cursor is the same
+              -- as the current suggestion, so we should advance
+              -- to the next completion and try again.
+              let complIndex' = nextComplIndex st (length l)
+                  highlightedCompl' = highlightedItem st { complIndex = complIndex'} cs
+              hlComplete l $ st { complIndex = complIndex',
+                                  highlightedCompl = highlightedCompl'
+                                }
+            else do
+              -- The word under the cursor differs from the
+              -- current suggestion, so replace it
+              put st
+              killWord Prev
+              insertString' newCommand
+              endOfLine
 -- | Initiate a prompt sub-map event loop. Submaps are intended to provide
 -- alternate keybindings. Accepts a default action and a mapping from key
 -- combinations to actions. If no entry matches, the default action is run.
@@ -1189,10 +1186,15 @@ resetComplIndex st = if (alwaysHighlight $ config st) then st { complIndex = (0,
 
 -- | Insert a character at the cursor position
 insertString :: String -> XP ()
-insertString str =
+insertString str = do
+  insertString' str
+  modify resetComplIndex
+
+insertString' :: String -> XP ()
+insertString' str =
   modify $ \s -> let
     cmd = (c (command s) (offset s))
-    st = resetComplIndex $ s { offset = o (offset s)}
+    st = s { offset = o (offset s)}
     in setCommand cmd st
   where o oo = oo + length str
         c oc oo | oo >= length oc = oc ++ str

commit ee4a3a932da7c10425108428e2ef87aa51785d8e

XMonad.Prompt: Fix alwaysHighlight tab-completion
526336ecec98aa2fe1cc98521473372b0664e19d introduced a bug where
tab-completion has an issue when a completion contains spaces; it only
deletes the last word of that completion, causing the next TAB to again
select the current selection and add it to the command.  In this way,
the prompt can get "stuck" on an item, endlessly adding all but the
first word to the command every time TAB is pressed.

C.f. xmonad/xmonad-contrib/issues/455

XMonad/Prompt.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -100,7 +101,7 @@ import           XMonad.Util.Types
 import           XMonad.Util.XSelection       (getSelection)
 
 import           Codec.Binary.UTF8.String     (decodeString,isUTF8Encoded)
-import           Control.Arrow                (first, second, (&&&), (***))
+import           Control.Arrow                (first, (&&&), (***))
 import           Control.Concurrent           (threadDelay)
 import           Control.Exception            as E hiding (handle)
 import           Control.Monad.State
@@ -425,9 +426,6 @@ command = W.focus . commandHistory
 setCommand :: String -> XPState -> XPState
 setCommand xs s = s { commandHistory = (commandHistory s) { W.focus = xs }}
 
-setHighlightedCompl :: Maybe String -> XPState -> XPState
-setHighlightedCompl hc st = st { highlightedCompl = hc}
-
 -- | Sets the input string to the given value.
 setInput :: String -> XP ()
 setInput = modify . setCommand
@@ -729,9 +727,9 @@ handleCompletion cs = do
     st <- get
 
     let updateWins  l = redrawWindows l
-        updateState l = case alwaysHlight of
-            False -> simpleComplete l st
-            True  -> hlComplete l st
+        updateState l = if alwaysHlight
+            then hlComplete (getLastWord $ command st) l st
+            else simpleComplete                        l st
 
     case cs of
       []  -> updateWindows
@@ -756,26 +754,37 @@ handleCompletion cs = do
         -- buffer to reflect that.
         --
         --TODO: Scroll or paginate results
-        hlComplete :: [String] -> XPState -> XP ()
-        hlComplete l st =
-          let newCommand = fromMaybe (command st) $ highlightedItem st l in
-          if newCommand == (getLastWord . command $ st)
-            then do
-              -- The word currently under the cursor is the same
-              -- as the current suggestion, so we should advance
-              -- to the next completion and try again.
-              let complIndex' = nextComplIndex st (length l)
-                  highlightedCompl' = highlightedItem st { complIndex = complIndex'} cs
-              hlComplete l $ st { complIndex = complIndex',
-                                  highlightedCompl = highlightedCompl'
-                                }
-            else do
-              -- The word under the cursor differs from the
-              -- current suggestion, so replace it
+        hlComplete :: String -> [String] -> XPState -> XP ()
+        hlComplete prevCompl l st =
+          if | -- The current suggestion matches the command and is a
+               -- proper suffix of the last suggestion, so replace it.
+               isSuffixOfCmd && isProperSuffixOfLast -> replaceCompletion
+
+               -- The current suggestion matches the command, so advance
+               -- to the next completion and try again.
+             | isSuffixOfCmd ->
+                 hlComplete hlCompl l $ st{ complIndex = complIndex'
+                                          , highlightedCompl = nextHlCompl
+                                          }
+
+               -- If nothing matches at all, delete the suggestion and
+               -- highlight the next one.
+             | otherwise -> replaceCompletion
+         where
+          hlCompl     :: String       = fromMaybe (command st) $ highlightedItem st l
+          complIndex' :: (Int, Int)   = nextComplIndex st (length l)
+          nextHlCompl :: Maybe String = highlightedItem st{ complIndex = complIndex' } cs
+
+          isSuffixOfCmd        :: Bool = hlCompl `isSuffixOf` command st
+          isProperSuffixOfLast :: Bool =      hlCompl   `isSuffixOf` prevCompl
+                                      && not (prevCompl `isSuffixOf` hlCompl)
+
+          replaceCompletion :: XP () = do
               put st
-              killWord Prev
-              insertString' newCommand
+              replicateM_ (length $ words prevCompl) $ killWord Prev
+              insertString' hlCompl
               endOfLine
+
 -- | Initiate a prompt sub-map event loop. Submaps are intended to provide
 -- alternate keybindings. Accepts a default action and a mapping from key
 -- combinations to actions. If no entry matches, the default action is run.