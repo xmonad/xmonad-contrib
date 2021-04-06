xmonad/xmonad-contrib

commit f2cfaa33980061f4fb39b689403701d36babe33f

Safe indexing in 'highlightedItem'
Just because there are some completions doesn't mean that the given
index may not still be too big.

Incidentally, this now allows every prompt to cycle through its items in
a nice way.

XMonad/Prompt.hs
@@ -107,7 +107,7 @@ import           Data.Char                    (isSpace)
 import           Data.IORef
 import           Data.List
 import qualified Data.Map                     as M
-import           Data.Maybe                   (fromMaybe)
+import           Data.Maybe                   (fromMaybe, listToMaybe)
 import           Data.Set                     (fromList, toList)
 import           System.IO
 import           System.IO.Unsafe             (unsafePerformIO)
@@ -397,10 +397,14 @@ highlightedItem st' completions = case complWinDim st' of
     let
       (_,_,_,_,xx,yy) = winDim
       complMatrix = splitInSubListsAt (length yy) (take (length xx * length yy) completions)
-      (col_index,row_index) = (complIndex st')
+      (col_index,row_index) = complIndex st'
     in case completions of
       [] -> Nothing
-      _ -> Just $ complMatrix !! col_index !! row_index
+      _  -> complMatrix !? col_index >>= (!? row_index)
+ where
+  -- | Safe version of '(!!)'.
+  (!?) :: [a] -> Int -> Maybe a
+  (!?) xs n = listToMaybe $ drop n xs
 
 -- | Return the selected completion, i.e. the 'String' we actually act
 -- upon after the user confirmed their selection (by pressing @Enter@).

commit 780360abf078d47908c446941bc0c0bae6b8664d

X.Prompt: Special handling of one highlighted item
Fixes a bug introduced in f2cfaa33980061f4fb39b689403701d36babe33f.

The changes there allowed the prompt to cycle through its input; it now
becomes necessary to single out the case of only having a single
suggestion that's also highlighted.  Otherwise, `hlComplete` (condition:
`alwaysHighlight` is enabled) will loop forever.  This case can't be
handled from within hlComplete, as we are giving it incomplete
information to start with (by only calling it with the last word of the
current completion in `handleCompletion`), which is necessary for things
like completing arguments.

XMonad/Prompt.hs
@@ -410,9 +410,10 @@ highlightedItem st' completions = case complWinDim st' of
       (_,_,_,_,xx,yy) = winDim
       complMatrix = splitInSubListsAt (length yy) (take (length xx * length yy) completions)
       (col_index,row_index) = complIndex st'
-    in case completions of
-      [] -> Nothing
-      _  -> complMatrix !? col_index >>= (!? row_index)
+    in case length completions of
+      0 -> Nothing
+      1 -> Just $ complMatrix !! col_index !! row_index
+      _ -> complMatrix !? col_index >>= (!? row_index)
  where
   -- | Safe version of '(!!)'.
   (!?) :: [a] -> Int -> Maybe a