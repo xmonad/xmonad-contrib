xmonad/xmonad-contrib

commit bd5b969d9ba24236c0d5ef521c0397390dbc4b37

Apply hlint hints
All hints are applied in one single commit, as a commit per hint would
result in 80+ separate commits—tihs is really just too much noise.

Related: https://github.com/xmonad/xmonad-contrib/issues/537

scripts/xmonadctl.hs
@@ -33,14 +33,13 @@ parse input addr args = case args of
 
 repl :: String -> IO ()
 repl addr = do e <- isEOF
-               case e of
-                True -> return ()
-                False -> do l <- getLine
-                            sendCommand addr l
-                            repl addr
+               unless e $ do
+                 l <- getLine
+                 sendCommand addr l
+                 repl addr
 
 sendAll :: String -> [String] -> IO ()
-sendAll addr ss = foldr (\a b -> sendCommand addr a >> b) (return ()) ss
+sendAll addr = foldr (\a b -> sendCommand addr a >> b) (return ())
 
 sendCommand :: String -> String -> IO ()
 sendCommand addr s = do

commit 6ab136eb5606ddec0735bda0be5b82785494a409

scripts/xmonadctl: Fix build
Fixes: bd5b969d9ba2 ("Apply hlint hints")

scripts/xmonadctl.hs
@@ -6,11 +6,12 @@
 -- Compile with @ghc --make xmonadctl.hs@
 -- For usage help, do @xmonadctl -h@
 
+import Control.Monad
+import Data.Char
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras
 import System.Environment
 import System.IO
-import Data.Char
 
 main :: IO ()
 main = parse True "XMONAD_COMMAND" =<< getArgs