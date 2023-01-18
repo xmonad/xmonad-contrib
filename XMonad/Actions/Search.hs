xmonad/xmonad-contrib

commit 7665b67ff4f90343324ad11618324892a902b184

X.A.Search: Add noogle

XMonad/Actions/Search.hs
@@ -164,6 +164,8 @@ import           XMonad.Util.XSelection   (getSelection)
 
 * 'ncatlab' -- Higer Algebra, Homotopy and Category Theory Wiki.
 
+* 'noogle' -- 'hoogle'-like Nix API search engine.
+
 * 'openstreetmap' -- OpenStreetMap free wiki world map.
 
 * 'protondb' -- Steam Proton Game Database.
@@ -333,7 +335,7 @@ searchEngineF = SearchEngine
 alpha, amazon, arXiv, aur, clojureDocs, codesearch, cratesIo, deb, debbts, debpts, dictionary, duckduckgo, ebay, flora,
   github, google, hackage, hoogle, images, imdb, lucky, maps, mathworld, ncatlab, openstreetmap, protondb, rosettacode, rustStd,
   scholar, sourcehut, stackage, steam, thesaurus, vocabulary, voidpgks_x86_64, voidpgks_x86_64_musl, wayback, wikipedia, wiktionary,
-  youtube, zbmath :: SearchEngine
+  youtube, zbmath, noogle :: SearchEngine
 alpha         = searchEngine "alpha"         "https://www.wolframalpha.com/input/?i="
 amazon        = searchEngine "amazon"        "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="
 arXiv         = searchEngineF "arXiv"        (\s -> "https://arxiv.org/search/?query=" <> s <> "&searchtype=all")
@@ -358,6 +360,7 @@ lucky         = searchEngine "lucky"         "https://www.google.com/search?btnI
 maps          = searchEngine "maps"          "https://maps.google.com/maps?q="
 mathworld     = searchEngine "mathworld"     "https://mathworld.wolfram.com/search/?query="
 ncatlab       = searchEngine "ncatlab"       "https://ncatlab.org/nlab/search?query="
+noogle        = searchEngineF "noogle"       (\s -> "https://noogle.dev/?search=" <> s <> "&page=1&to=any&from=any")
 openstreetmap = searchEngine "openstreetmap" "https://www.openstreetmap.org/search?query="
 protondb      = searchEngine "protondb"      "https://www.protondb.com/search?q="
 rosettacode   = searchEngine "rosettacode"   "https://rosettacode.org/w/index.php?search="

commit df1fc2d33408e96a347da54dc86ca28cec44a308

X.A.Search: Export noogle
Fixes: 7665b67ff4f90343324ad11618324892a902b184

XMonad/Actions/Search.hs
@@ -55,6 +55,7 @@ module XMonad.Actions.Search (   -- * Usage
                                  maps,
                                  mathworld,
                                  ncatlab,
+                                 noogle,
                                  openstreetmap,
                                  protondb,
                                  rosettacode,