6e5e82805d39dc3086ebb2aaf83328985c5a6792
Zawaken <zawaken@outlook.com>
2023-07-28 23:37:31 +0200

    Export 'Mag' and 'Toggle' from Magnifier

--- XMonad/Layout/Magnifier.hs
+++ XMonad/Layout/Magnifier.hs
@@ -43,7 +43,8 @@ module XMonad.Layout.Magnifier
       -- * Messages and Types
       MagnifyMsg (..),
       MagnifyThis(..),
-      Magnifier,
+      Magnifier(..),
+      Toggle(..),
     ) where
 
 import Numeric.Natural (Natural)
