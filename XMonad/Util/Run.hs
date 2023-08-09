xmonad/xmonad-contrib

commit 1b7fd6d8c99eee0b99261e48ed030b2ba42c88b5

X.U.Run: Add >&&> and >||>

XMonad/Util/Run.hs
@@ -60,6 +60,8 @@ module XMonad.Util.Run (
   -- ** General Combinators
   (>->),
   (>-$),
+  (>&&>),
+  (>||>),
   inWorkingDir,
   eval,
   execute,
@@ -361,6 +363,24 @@ infixr 3 >->
 (>-$) xi xs = xi >-> fmap mkDList xs
 infixr 3 >-$
 
+-- | @a >&&> b@ glues the different inputs @a@ and @b@ by means of @&&@.
+-- For example,
+--
+-- @
+-- pure "do something" >&&> pure "do another thing"
+-- @
+--
+-- would result in @do something && do another thing@ being executed by a
+-- shell.
+(>&&>) :: X Input -> X Input -> X Input
+a >&&> b = comp3 <$> a <*> toInput " && " <*> b
+infixr 2 >&&>
+
+-- | Like '(>&&>)', but with @||@.
+(>||>) :: X Input -> X Input -> X Input
+a >||> b = comp3 <$> a <*> toInput " || " <*> b
+infixr 2 >||>
+
 -- | Spawn a completed input.
 proc :: X Input -> X ()
 proc xi = spawn =<< getInput xi
@@ -540,3 +560,6 @@ tryQuote :: String -> String
 tryQuote s = case dropWhile (== ' ') s of
   '\'' : _ -> s
   _        -> "'" <> s <> "'"
+
+comp3 :: (c -> d) -> (b -> c) -> (a -> b) -> a -> d
+comp3 f g h = f . g . h

commit 348df92b893367dd504f3a5f2877537af292eee4

X.U.Run: Fix definitions of >&&> and >||>
Whoops.

Fixes: 1b7fd6d8c99eee0b99261e48ed030b2ba42c88b5

XMonad/Util/Run.hs
@@ -373,12 +373,12 @@ infixr 3 >-$
 -- would result in @do something && do another thing@ being executed by a
 -- shell.
 (>&&>) :: X Input -> X Input -> X Input
-a >&&> b = comp3 <$> a <*> toInput " && " <*> b
+a >&&> b = a <> toInput " && " <> b
 infixr 2 >&&>
 
 -- | Like '(>&&>)', but with @||@.
 (>||>) :: X Input -> X Input -> X Input
-a >||> b = comp3 <$> a <*> toInput " || " <*> b
+a >||> b = a <> toInput " || " <> b
 infixr 2 >||>
 
 -- | Spawn a completed input.
@@ -560,6 +560,3 @@ tryQuote :: String -> String
 tryQuote s = case dropWhile (== ' ') s of
   '\'' : _ -> s
   _        -> "'" <> s <> "'"
-
-comp3 :: (c -> d) -> (b -> c) -> (a -> b) -> a -> d
-comp3 f g h = f . g . h